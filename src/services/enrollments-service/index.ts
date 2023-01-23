c6439ce9707ac38c29af64a49bf2b6e0460c8d61
Josana-Kla <josana.0205@gmail.com>
2023-01-22 23:16:12 -0300

    feat: getting address from cep

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -5,12 +5,22 @@ import enrollmentRepository, { CreateEnrollmentParams } from "@/repositories/enr
 import { exclude } from "@/utils/prisma-utils";
 import { Address, Enrollment } from "@prisma/client";
 
-async function getAddressFromCEP() {
-  const result = await request.get("https://viacep.com.br/ws/37440000/json/");
+async function getAddressFromCEP(cep: string) {
+  const result = await request.get(`https://viacep.com.br/ws/${cep}/json/`);
 
   if (!result.data) {
     throw notFoundError();
   }
+
+  const addressData = {
+    logradouro: result.data.logradouro, 
+    complemento: result.data.complemento, 
+    bairro: result.data.bairro, 
+    cidade: result.data.localidade, 
+    uf: result.data.uf,
+  };
+
+  return addressData;
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
